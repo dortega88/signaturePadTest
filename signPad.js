c3a981870ab5e1c7724d5b5578cbbd16c705d57d
Daniel Ortega <dortega88@gmail.com>
2019-06-06 15:56:28 -0500

    Added css and drawSignatureLine()

--- signPad.js
+++ signPad.js
@@ -1,24 +1,42 @@
-$(function(){
-    drawSignatureLine();
-    var canvas = document.getElementById('signature-pad');
-    var signaturePad = new SignaturePad(canvas);
-    drawSignatureLine();
 
-    $('button.save').click(function(){
-        window.open(signaturePad.toDataURL("image/png"))
-        alert("saved");
-    });
-    $('button.clear').click(function(){
-        signaturePad.clear();
-        drawSignatureLine();
-    });
-    function drawSignatureLine(){
-          var context = canvas.getContext('2d');
-          context.lineWidth = .5;
-          context.strokeStyle = '#333';
-          context.beginPath();
-          context.moveTo(0, 150);
-          context.lineTo(500, 150);
-          context.stroke();
+jQuery(document).ready(function($) {
+  $(function(){
+    drawSignatureLine();
+  })
+
+  var canvas = document.getElementById("signature-pad");
+
+  var signaturePad = new SignaturePad(canvas, {
+    backgroundColor: "rgb(255, 0, 0)",
+    penColor: "rgb(255, 0, 0)",
+    enableTypeToSign: true,
+    inputSelector: "#txt_name",
+    maxFontSize: "28",  // Defaults to 300 pt.
+    fontface: "Arial" // Defaults to Helvetica
+  });
+
+
+
+  $('button.save').click(function() {
+    window.open(signaturePad.toDataURL("image/png"))
+    alert("saved");
+    drawSignatureLine();
+  });
+
+
+  document.getElementById('clear').addEventListener('click', function () {
+      signaturePad.clear();
+      $('#txt_name').val('');
+    drawSignatureLine();
+  });
+
+  function drawSignatureLine() {
+     var context = canvas.getContext('2d');
+     context.lineWidth = .5;
+     context.strokeStyle = '#333';
+     context.beginPath();
+     context.moveTo(0, 150);
+     context.lineTo(500, 150);
+     context.stroke();
     }
 });
